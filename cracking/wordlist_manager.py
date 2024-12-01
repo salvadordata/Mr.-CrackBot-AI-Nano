ffdaba99c514e1e99bae6ca8335051c634e98e05
salvadordata <ak987tbram@icloud.com>
2024-11-30 22:33:08 -0500

    Update wordlist_manager.py

--- cracking/wordlist_manager.py
+++ cracking/wordlist_manager.py
@@ -1,1 +1,13 @@
+from ai.password_model import PasswordGenerator
 
+class WordlistManager:
+    def __init__(self):
+        self.generator = PasswordGenerator()
+
+    def generate_wordlist(self, features):
+        prompt = f"SSID: {features['ssid']}, BSSID: {features['bssid_prefix']}"
+        words = self.generator.generate(prompt)
+        wordlist_path = "data/wordlists/generated.txt"
+        with open(wordlist_path, "w") as file:
+            file.write("\n".join(words))
+        return wordlist_path
