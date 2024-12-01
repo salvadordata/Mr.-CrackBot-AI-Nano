b4c933cc41aa1d805c747df4faadad0b353b239e
salvadordata <ak987tbram@icloud.com>
2024-11-30 22:33:47 -0500

    Update hashcat_wrapper.py

--- cracking/hashcat_wrapper.py
+++ cracking/hashcat_wrapper.py
@@ -1,1 +1,5 @@
+import subprocess
 
+def crack_password(handshake_file, wordlist_file):
+    command = f"hashcat -m 2500 {handshake_file} {wordlist_file} --force"
+    subprocess.run(command, shell=True)
